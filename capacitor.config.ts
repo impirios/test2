d348bfee1752c40550281a1bb3a6ac2b5517b0e9
impirios <helloworldwastaken@gmail.com>
2022-02-14 00:42:32 +0530

    ahhh last try for the night

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.example.mytestApplication',
+  appName: 'Hot',
+  webDir: 'dist',
+  bundledWebRuntime: false
+};
+
+export default config;
